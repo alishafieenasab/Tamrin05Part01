667660ef68c1377bdf8e9eb5ba15bf7d613b4b9a
Ali <Ali@DESKTOP-91LMIMA>
2022-04-10 07:37:47 +0430

    Add project files.

---
+++ Tamrin05Part01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+string? filePath = @"C:\Users\Ali\source\repos\Tamrin05Part01";
+string? destFilePath = @"C:\Users\Ali\source\repos\Tamrin05Part01\Archive";
+try
+{
+    File.Copy(filePath, destFilePath, false);
+    Console.WriteLine("File Copied Successfully!");
+    Console.WriteLine("Do you want to make your file secure from writing?y/n");
+    var op = Console.ReadLine();
+    if (op == "y")
+    {
+        File.SetAttributes(destFilePath, (new FileInfo(destFilePath)).Attributes | FileAttributes.ReadOnly);
+    }
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine("The Directory Doesn't Found!");
+}
+catch (IOException ex)
+{
+    Console.WriteLine("The File already exist");
+}
+catch (UnauthorizedAccessException)
+{
+    Console.WriteLine("Unable To Write on The File");
+}
+catch
+{
+    Console.WriteLine("There is an Unknowen Error!");
+}
